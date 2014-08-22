e9d3b7a1ec74985d691c2601463c58566a690ef2
Anthony Picciano <anthony.picciano@gmail.com>
2014-08-22 11:31:12 -0600

    Initial Commit

---
+++ CollectionViewLaunchAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewLaunchAnimation
+//
+//  Created by Anthony Picciano on 8/22/14.
+//  Copyright (c) 2014 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewLaunchAnimationTests/CollectionViewLaunchAnimationTests.m
@@ -0,0 +1,34 @@
+//
+//  CollectionViewLaunchAnimationTests.m
+//  CollectionViewLaunchAnimationTests
+//
+//  Created by Anthony Picciano on 8/22/14.
+//  Copyright (c) 2014 Anthony Picciano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CollectionViewLaunchAnimationTests : XCTestCase
+
+@end
+
+@implementation CollectionViewLaunchAnimationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
